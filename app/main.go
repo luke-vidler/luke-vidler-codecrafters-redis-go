595362ff1b0766cb15df413ef00a41e9b3842b58
Luke Vidler <luke.vidler@gmail.com>
2025-09-06 14:49:14 +1000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,20 +10,8 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+func handleClient(conn net.Conn) {
+	defer conn.Close()
 
 	for {
 		buffer := make([]byte, 1024)
@@ -34,5 +22,26 @@ func main() {
 		}
 
 		conn.Write([]byte("+PONG\r\n"))
+	}
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+
+		go handleClient(conn)
 	}
 }
