a5f69fce5a9cbd73adc8a89ddc65db4cdb7028bc
Luke Vidler <luke.vidler@gmail.com>
2025-09-06 14:54:09 +1000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,27 +1,92 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
+func parseRESP(reader *bufio.Reader) ([]string, error) {
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+
+	line = strings.TrimRight(line, "\r\n")
+
+	if !strings.HasPrefix(line, "*") {
+		return nil, fmt.Errorf("expected array, got %s", line)
+	}
+
+	arrayLen, err := strconv.Atoi(line[1:])
+	if err != nil {
+		return nil, err
+	}
+
+	args := make([]string, arrayLen)
+
+	for i := 0; i < arrayLen; i++ {
+		line, err = reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+
+		line = strings.TrimRight(line, "\r\n")
+
+		if !strings.HasPrefix(line, "$") {
+			return nil, fmt.Errorf("expected bulk string, got %s", line)
+		}
+
+		strLen, err := strconv.Atoi(line[1:])
+		if err != nil {
+			return nil, err
+		}
+
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+
+		args[i] = strings.TrimRight(str, "\r\n")[:strLen]
+	}
+
+	return args, nil
+}
+
 func handleClient(conn net.Conn) {
 	defer conn.Close()
+	reader := bufio.NewReader(conn)
 
 	for {
-		buffer := make([]byte, 1024)
-		_, err := conn.Read(buffer)
+		args, err := parseRESP(reader)
 		if err != nil {
-			fmt.Println("Error reading from connection: ", err.Error())
+			fmt.Println("Error parsing RESP: ", err.Error())
 			break
 		}
 
-		conn.Write([]byte("+PONG\r\n"))
+		if len(args) == 0 {
+			continue
+		}
+
+		command := strings.ToUpper(args[0])
+
+		switch command {
+		case "PING":
+			conn.Write([]byte("+PONG\r\n"))
+		case "ECHO":
+			if len(args) >= 2 {
+				arg := args[1]
+				response := fmt.Sprintf("$%d\r\n%s\r\n", len(arg), arg)
+				conn.Write([]byte(response))
+			}
+		}
 	}
 }
 
